6a390091025ede6bf0c53706e40d79a9d964d80b
Marcel Hild <hild@b4mad.net>
2022-10-27 17:02:35 +0200

    dash

--- pitcrew/app.py
+++ pitcrew/app.py
@@ -6,9 +6,39 @@ from coach import Coach
 from history import History
 from mqtt import Mqtt
 
+from dash import Dash, html, dcc, Input, Output
+import dash_bootstrap_components as dbc
+
 logging.basicConfig(level=logging.DEBUG)
 
-if __name__ == "__main__":
+# app = Dash(__name__)
+app = Dash(external_stylesheets=[dbc.themes.VAPOR])
+
+markdown_text = """
+# B4MAD Racing Pit Crew
+"""
+
+app.layout = html.Div(
+    [
+        dcc.Markdown(children=markdown_text),
+        dcc.RadioItems(
+            ["on", "off"],
+            id="power",
+        ),
+        html.Hr(),
+        html.Div(id="power-state"),
+    ]
+)
+
+
+@app.callback(Output("power-state", "children"), Input("power", "value"))
+def set_display_children(state):
+    return "Power is {}".format(
+        state,
+    )
+
+
+def headless():
     history = History()
     coach = Coach(history)
     mqtt = Mqtt(coach)
@@ -20,3 +50,11 @@ if __name__ == "__main__":
     h = threading.Thread(target=history_thread)
     h.start()
     mqtt.run()
+
+
+if __name__ == "__main__":
+    h = threading.Thread(target=headless)
+    h.start()
+
+    # https://stackoverflow.com/a/68851873
+    app.run_server(debug=False)
