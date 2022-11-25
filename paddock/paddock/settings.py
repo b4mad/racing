8c709c039d76a4d62c680dc7635869a11d2f5a67
Christoph Görn <goern@b4mad.net>
2022-11-25 15:45:05 +0100

    use random secret key
    Signed-off-by: Christoph Görn <goern@b4mad.net>

--- paddock/paddock/settings.py
+++ paddock/paddock/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+from django.core.management.utils import get_random_secret_key
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-m@azq(w=7f-e79a1t@h3m_r5&k4&_51nor(uk==9ni1#63)y0!"
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
