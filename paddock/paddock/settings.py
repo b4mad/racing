45733f7d197a07e1680fe2a9c1b84fe779701aa1
Christoph Görn <goern@b4mad.net>
2022-11-25 15:51:54 +0100

    add django db config from env
    Signed-off-by: Christoph Görn <goern@b4mad.net>

--- paddock/paddock/settings.py
+++ paddock/paddock/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 from django.core.management.utils import get_random_secret_key
@@ -77,10 +79,18 @@ WSGI_APPLICATION = "paddock.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
+    "sqlite3": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
-    }
+    },
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_DATABASE"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_ADDR"),
+        "PORT": os.getenv("DB_PORT", 5432),
+    },
 }
 
 
