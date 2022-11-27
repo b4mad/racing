cad28159c9ecc821060d1f29f2044834c9c4c002
Marcel Hild <hild@b4mad.net>
2022-11-27 09:49:33 +0100

    comment delete

--- components/paddock/paddock/settings.py
+++ components/paddock/paddock/settings.py
@@ -26,7 +26,6 @@ env = environ.Env(
 
 # Set the project base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Take environment variables from .env file
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
