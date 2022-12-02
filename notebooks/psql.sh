301b910108fe8c1a20bff68a04f702cc6b681f08
Marcel Hild <hild@b4mad.net>
2022-12-02 11:13:41 +0100

    test fast lap analysis

--- notebooks/psql.sh
+++ notebooks/psql.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 PG_CLUSTER_USER_SECRET_NAME=b4mad-racing-pguser-free-practice
+PG_CLUSTER_USER_SECRET_NAME=b4mad-racing-pguser-paddock
 
 PGPASSWORD=$(kubectl get secrets -n b4mad-racing "${PG_CLUSTER_USER_SECRET_NAME}" -o go-template='{{.data.password | base64decode}}') \
 PGUSER=$(kubectl get secrets -n b4mad-racing "${PG_CLUSTER_USER_SECRET_NAME}" -o go-template='{{.data.user | base64decode}}') \
