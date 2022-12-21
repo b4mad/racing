24b5c4acd1f8d0890cd3a04b94400f4eecd1f481
Marcel Hild <hild@b4mad.net>
2022-12-21 15:40:35 +0100

    notebooks bonanza

--- notebooks/psql.sh
+++ notebooks/psql.sh
@@ -2,6 +2,7 @@
 
 PG_CLUSTER_USER_SECRET_NAME=b4mad-racing-pguser-free-practice
 PG_CLUSTER_USER_SECRET_NAME=b4mad-racing-pguser-paddock
+PG_CLUSTER_USER_SECRET_NAME=b4mad-racing-pguser-paddock-root
 
 PGPASSWORD=$(kubectl get secrets -n b4mad-racing "${PG_CLUSTER_USER_SECRET_NAME}" -o go-template='{{.data.password | base64decode}}') \
 PGUSER=$(kubectl get secrets -n b4mad-racing "${PG_CLUSTER_USER_SECRET_NAME}" -o go-template='{{.data.user | base64decode}}') \
