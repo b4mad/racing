68b39de1a01837fce445944ade1d44ff489855d3
Marcel Hild <hild@b4mad.net>
2023-06-22 17:08:06 +0200

    fix tests

--- components/paddock/telemetry/fast_lap_analyzer.py
+++ components/paddock/telemetry/fast_lap_analyzer.py
@@ -107,21 +107,6 @@ class FastLapAnalyzer:
 
         return data, list(used_laps)
 
-    # def analyze_df(self, df):
-    #     self._df = self.preprocess(df)
-    #     if self._df is None:
-    #         return
-
-    #     segments = self.get_segments(self._df)
-    #     # logging.info(track_info_df.style.format(precision=1).to_string())
-
-    #     distance_time = self.analyzer.distance_speed_lookup_table(self._df)
-    #     data = {
-    #         "distance_time": distance_time,
-    #         "segments": segments,
-    #     }
-    #     return data
-
     def brake_features(self, df):
         brake_feature_args = {
             "brake_threshold": 0.1,
@@ -167,45 +152,6 @@ class FastLapAnalyzer:
             segment.add_features(gear_features, type="gear")
         segment.telemetry = sector
         return segment
-
-    # def get_segments(self, track_df):
-    #     analyzer = self.analyzer
-    #     sectors = analyzer.split_sectors(track_df, min_length=50)
-    #     sector_start_end = analyzer.extract_sector_start_end(sectors, min_length=50)
-    #     track_info = []
-    #     for i in range(len(sector_start_end)):
-    #         sector = analyzer.section_df(track_df, sector_start_end[i]["start"], sector_start_end[i]["end"])
-
-    #         throttle_or_brake = analyzer.sector_type(sector)
-    #         brake_features = self.brake_features(sector)
-    #         throttle_features = self.throttle_features(sector)
-    #         gear_features = self.gear_features(sector)
-
-    #         segment = Segment()
-    #         segment.type = throttle_or_brake
-    #         segment.turn = i + 1
-
-    #         speed = 0
-    #         if throttle_or_brake == "brake" and brake_features:
-    #             start = brake_features["start"]
-    #             speed = analyzer.value_at_distance(sector, start, column="SpeedMs")
-    #             brake_features["speed"] = speed
-    #         elif throttle_or_brake == "throttle" and throttle_features:
-    #             start = throttle_features["start"]
-    #             speed = analyzer.value_at_distance(sector, start, column="SpeedMs")
-
-    #         if brake_features:
-    #             segment.add_features(brake_features, type="brake")
-    #         if throttle_features:
-    #             segment.add_features(throttle_features, type="throttle")
-    #         if gear_features:
-    #             segment.add_features(gear_features, type="gear")
-    #         segment.telemetry = sector
-    #         segment.start = sector_start_end[i]["start"]
-    #         segment.end = sector_start_end[i]["end"]
-
-    #         track_info.append(segment)
-    #     return track_info
 
     def preprocess(self, df):
         # # Check if the value is increasing compared to the previous value
