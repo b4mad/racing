71e30a4f65a68657aaa513f498389d0562c92fa3
Marcel Hild <hild@b4mad.net>
2023-07-04 18:14:42 +0200

    update fixtures and tests

--- components/paddock/scripts/dumpdata.sh
+++ components/paddock/scripts/dumpdata.sh
@@ -3,8 +3,8 @@
 set -x
 cd "$(dirname "$0")/.."
 
-# models="game car track sessiontype session driver coach fastlap fastlapsegment lap"
-models="lap"
+models="game car track sessiontype session driver coach fastlap fastlapsegment lap"
+models="fastlapsegment lap"
 for o in $models; do
   pipenv run ./manage.py dumpdata --indent 2 telemetry.$o > telemetry/fixtures.all/$o.json
 done
--- components/paddock/telemetry/fixtures/filter.sh
+++ components/paddock/telemetry/fixtures/filter.sh
@@ -4,11 +4,11 @@ cd $(dirname $0)
 jq '[.[] | select(.pk == 1 or .pk == 5)]' ../fixtures.all/game.json > game.json
 # jq '[.[] | select(.fields.game == 1 or .fields.game == 5)]' ../fixtures.all/car.json > car.json
 jq '[.[] | select(.pk == 9 or .pk == 351)]' ../fixtures.all/car.json > car.json
-jq '[.[] | select(.pk == 409 or .pk == 83 or .pk == 1040)]' ../fixtures.all/track.json > track.json
+jq '[.[] | select(.pk == 409 or .pk == 83 or .pk == 1040 or .pk == 193)]' ../fixtures.all/track.json > track.json
 jq '[.[] | select(.pk == 10 )]' ../fixtures.all/coach.json > coach.json
 jq '[.[] | select(.pk == 10 or .pk == 1)]' ../fixtures.all/driver.json > driver.json
-jq '[.[] | select(.pk == 703 or .pk == 157 or .pk == 1237)]' ../fixtures.all/fastlap.json > fastlap.json
+jq '[.[] | select(.pk == 703 or .pk == 157 or .pk == 1237 or .pk == 15)]' ../fixtures.all/fastlap.json > fastlap.json
 jq '[.[] | select(.fields.fast_lap == 703 or .fields.fast_lap == 157 or .fields.fast_lap == 1237)]' ../fixtures.all/fastlapsegment.json > fastlapsegment.json
 jq '[.[] | select(.fields.fast_lap == 703 or .fields.fast_lap == 157 or .fields.fast_lap == 1237)]' ../fixtures.all/lap.json > lap.json
-jq '[.[] | select(.pk == 24222 or .pk == 26684 or .pk == 57153)]' ../fixtures.all/session.json > session.json
+jq '[.[] | select(.pk == 24222 or .pk == 26684 or .pk == 57153 or .pk == 17096 or .pk == 17360 or .pk == 57010 or .pk == 57975 or .pk == 58151 or .pk == 58155)]' ../fixtures.all/session.json > session.json
 cp ../fixtures.all/sessiontype.json sessiontype.json
