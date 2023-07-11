77347b8eb0969469bead95fb37d37ee39cf94f79
Marcel Hild <hild@b4mad.net>
2023-07-11 10:04:54 +0200

    check for dead coaches

--- components/paddock/telemetry/pitcrew/coach_watcher.py
+++ components/paddock/telemetry/pitcrew/coach_watcher.py
@@ -35,6 +35,7 @@ class CoachWatcher:
 
     def watch_coaches(self):
         while True and not self.stopped():
+            self.check_active_coaches()
             # sleep longer than save_sessions, to make sure all DB objects are initialized
             drivers = self.drivers()
             # collect all driver names
@@ -57,7 +58,9 @@ class CoachWatcher:
     def stop_coach(self, driver_name):
         if driver_name not in self.active_coaches.keys():
             return
+        logging.info(f"disconnectin MQTT thread for {driver_name}")
         self.active_coaches[driver_name][0].disconnect()
+        logging.info(f"disconnecting History thread for {driver_name}")
         self.active_coaches[driver_name][1].disconnect()
         del self.active_coaches[driver_name]
 
@@ -89,7 +92,24 @@ class CoachWatcher:
         threads.append(c)
         c.start()
         h.start()
-        self.active_coaches[driver_name] = [history, mqtt]
+        self.active_coaches[driver_name] = [history, mqtt, threads]
+
+    def check_active_coaches(self):
+        dead_drivers = set()
+        for driver_name in self.active_coaches.keys():
+            # history = self.active_coaches[driver_name][0]
+            # mqtt = self.active_coaches[driver_name][1]
+            threads = self.active_coaches[driver_name][2]
+
+            for t in threads:
+                if not t.is_alive():
+                    self.stop()
+                    logging.error(f"Thread {t} died for {driver_name}")
+                    dead_drivers.add(driver_name)
+                    break
+
+        for driver_name in dead_drivers:
+            self.stop_coach(driver_name)
 
     def run(self):
         try:
