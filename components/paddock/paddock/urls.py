7ef33cf6662e34ab4f035928b2e8b2a5e03ab4e1
Christoph Görn <goern@b4mad.net>
2023-09-06 16:58:01 +0200

    🥵 hotfix the static_url to be included in urls
    Signed-off-by: Christoph Görn <goern@b4mad.net>

--- components/paddock/paddock/urls.py
+++ components/paddock/paddock/urls.py
@@ -3,6 +3,8 @@
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
@@ -29,4 +31,6 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("explorer/", include("explorer.urls")),
     path("django_plotly_dash/", include("django_plotly_dash.urls")),
-]
+] + static(
+    settings.STATIC_URL, document_root=settings.STATIC_ROOT
+)  # https://stackoverflow.com/questions/39907281/django-uwsgi-static-files-not-being-served-even-after-collectstatic
