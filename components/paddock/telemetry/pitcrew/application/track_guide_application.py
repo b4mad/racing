77d43559f72ad204870178ef2b023a96bab25df7
Marcel Hild <hild@b4mad.net>
2023-09-08 10:57:39 +0300

    detect coach style

--- components/paddock/telemetry/pitcrew/application/track_guide_application.py
+++ components/paddock/telemetry/pitcrew/application/track_guide_application.py
@@ -21,6 +21,12 @@ class TrackGuideApplication(Application):
 
     def tick(self):
         self.calculate_avg_speed()
+        if self.changed_coaching_style():
+            if self.is_recon_laps():
+                self.send_response("You are driving recon laps")
+            else:
+                self.send_response("You are driving race pace")
+
         if self.is_recon_laps():
             self.log_debug(f"recon laps - avg_speed_pct {self.avg_speed_pct}")
 
@@ -30,19 +36,22 @@ class TrackGuideApplication(Application):
             speed_pct = self.speed / race_pace_speed
             # Add current speed pct to history
             self.speed_pct_history.append(speed_pct)
-            self.log_debug(f"speed: {speed_pct}")
 
             if len(self.speed_pct_history) == self.speed_pct_history.maxlen:
                 # Queue is full, can calculate rolling average
                 self.avg_speed_pct = sum(self.speed_pct_history) / len(self.speed_pct_history)
 
-                if self.avg_speed_pct > 0.8:
-                    self.recon_laps = False
-                else:
-                    self.recon_laps = True
-
     def is_recon_laps(self):
         return self.recon_laps
+
+    def changed_coaching_style(self):
+        if self.is_recon_laps() and self.avg_speed_pct > 0.8:
+            self.recon_laps = False
+            return True
+        elif not self.is_recon_laps() and self.avg_speed_pct < 0.8:
+            self.recon_laps = True
+            return True
+        return False
 
     def on_reset_to_pits(self):
         self.send_response("TrackGuide: on_reset_to_pits")
