28d688f7e5130becdcd82acb992413d4392b0259
Marcel Hild <hild@b4mad.net>
2023-09-17 08:51:40 +0200

    add sentry

--- components/paddock/paddock/settings.py
+++ components/paddock/paddock/settings.py
@@ -7,6 +7,8 @@ import sys
 from pathlib import Path
 
 import environ
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 env = environ.Env(
     # set casting, default value
@@ -331,3 +333,21 @@ SOCIALACCOUNT_PROVIDERS = {
         }
     },
 }
+
+sentry_dsn = os.environ.get("SENTRY_DSN")
+if sentry_dsn and sentry_dsn.startswith("http"):
+    sentry_sdk.init(
+        dsn=sentry_dsn,
+        integrations=[DjangoIntegration()],
+        # If you wish to associate users to errors (assuming you are using
+        # django.contrib.auth) you may enable sending PII data.
+        send_default_pii=True,
+        # Set traces_sample_rate to 1.0 to capture 100%
+        # of transactions for performance monitoring.
+        # We recommend adjusting this value in production.
+        traces_sample_rate=1.0,
+        # Set profiles_sample_rate to 1.0 to profile 100%
+        # of sampled transactions.
+        # We recommend adjusting this value in production.
+        profiles_sample_rate=1.0,
+    )
