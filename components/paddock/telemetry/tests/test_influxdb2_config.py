ae09a8c33979db908687d39fe798d0f187573573
Christoph Görn <goern@b4mad.net>
2023-09-18 11:10:37 +0200

    ci: reworked test_getinfluxdb2_config
    Signed-off-by: Christoph Görn <goern@b4mad.net>

--- components/paddock/telemetry/tests/test_influxdb2_config.py
+++ components/paddock/telemetry/tests/test_influxdb2_config.py
@@ -1,32 +1,35 @@
 import os
 
 import pytest
-from django.test import TransactionTestCase
 
 from paddock.exceptions import RuntimeEnvironmentConfigurationIncompleteError
 from telemetry.utils import get_influxdb2_config
 
 
-class TestInfluxDB2Config(TransactionTestCase):
-    @pytest.fixture()
-    def setup(self):
-        os.environ["B4MAD_RACING_INFLUX_ORG"] = ""
-        os.environ["B4MAD_RACING_INFLUX_TOKEN"] = ""
-        os.environ["INFLUXDB2_SERVICE_HOST"] = ""
-        os.environ["INFLUXDB2_SERVICE_PORT"] = ""
-
+@pytest.mark.unittest
+class TestInfluxDB2Config:
     def test_getinfluxdb2_config(self):
         with pytest.raises(RuntimeEnvironmentConfigurationIncompleteError) as excinfo:
             (org, token, url) = get_influxdb2_config()
             assert org == "b4mad"
-        assert str(excinfo.value) == "missing environment variable: INFLUXDB2_SERVICE_HOST"
+        assert str(excinfo.value) == "missing environment variable: B4MAD_RACING_INFLUX_TOKEN"
 
         with pytest.raises(RuntimeEnvironmentConfigurationIncompleteError) as excinfo:
-            os.environ["INFLUXDB2_SERVICE_HOST"] = "test"
+            os.environ["B4MAD_RACING_INFLUX_TOKEN"] = "test"
 
             (org, token, url) = get_influxdb2_config()
 
             assert org == "b4mad"
-            assert url == "https://test:8086"
+            assert token == "test"
 
-        assert str(excinfo.value) == "missing environment variable: B4MAD_RACING_INFLUX_TOKEN"
+        assert str(excinfo.value) == "missing environment variable: INFLUXDB2_SERVICE_HOST"
+
+        os.environ["B4MAD_RACING_INFLUX_ORG"] = "test"
+        os.environ["B4MAD_RACING_INFLUX_TOKEN"] = "test"
+        os.environ["INFLUXDB2_SERVICE_HOST"] = "test"
+
+        (org, token, url) = get_influxdb2_config()
+
+        assert org == "test"
+        assert token == "test"
+        assert url == "https://test:8086"
