2dc1d014a58c104bed6fe757e4e2911c2fd43927
Marcel Hild <hild@b4mad.net>
2023-09-20 08:50:51 +0200

    recon laps

--- components/paddock/telemetry/pitcrew/application/track_guide_application.py
+++ components/paddock/telemetry/pitcrew/application/track_guide_application.py
@@ -1,5 +1,6 @@
 import datetime
 from collections import deque
+from typing import Dict
 
 from telemetry.models import TrackGuide
 
@@ -37,9 +38,13 @@ class Turn:
             self.set_next_response(distance)
             return response
 
-    def set_next_response(self, distance):
+    def current_response(self):
+        return self._current_response
+
+    def set_next_response(self, distance=0):
         self.responses.rotate(-1)
         self._current_response = self.responses[0]
+        return self._current_response
 
     def build_response(self, note):
         finish_at = False
@@ -75,8 +80,11 @@ class Turn:
 
         response = self.build_response(note)
         response.sort_key = f"{note.sort_key}"
+        self.responses.append(response)
+        self.sort_responses()
+
+    def sort_responses(self):
         responses = list(self.responses)
-        responses.append(response)
         responses.sort(key=lambda x: (x.sort_key, x.sort_at))
         self.responses = deque(responses)
         self._current_response = self.responses[0]
@@ -93,10 +101,45 @@ class Turn:
         else:
             raise Exception("No segment or landmark")
 
+    def reset(self, distance):
+        """find the first note that will be played
+        at this distance
+        """
+        if not self._current_response:
+            return
+
+        self.sort_responses()
+
+        # get all responses of the first batch
+        resp = []
+        sort_key = self._current_response.sort_key
+        while self._current_response.sort_key == sort_key:
+            resp.append(self._current_response)
+            self.set_next_response()
+
+        # sort by diff to distance
+        resp.sort(key=lambda x: abs(distance - x.at))
+        for response in resp:
+            if response.at > distance:
+                self.set_response(response)
+                break
+
+    def set_response(self, response):
+        self.sort_responses()
+        while self._current_response != response:
+            self.set_next_response()
+
 
 class TrackGuideApplication(Application):
+    MODE_UNDEFINED = 0
+    MODE_RECON = 1
+    MODE_PACE = 2
+
+    RECON_ADD_DISTANCE = 100
+
     def init(self):
-        self.recon_laps = True
+        self.mode = self.MODE_UNDEFINED
+        self.turns: Dict[str, Turn] = {}
 
         # get all notes for track and car
         self.track_guide: TrackGuide | None = TrackGuide.objects.filter(
@@ -110,7 +153,6 @@ class TrackGuideApplication(Application):
             self.init_turns()
 
     def init_turns(self):
-        self.turns = {}
         for note in self.track_guide.notes.all():
             turn_id = note.segment or note.landmark.name
             turn = self.turns.get(turn_id, None)
@@ -123,98 +165,10 @@ class TrackGuideApplication(Application):
             except Exception as e:
                 self.log_error(e)
 
-    def init_notes(self):
-        self.distance_notes = {}
-        self.segment_notes = {}
-        # store notes by segment / landmarks
-        for track_note in self.track_guide.notes.all():
-            note = Note(track_note)
-            finish_at = False
-
-            if track_note.segment:
-                segment = self.get_segment(turn=track_note.segment)
-                note.segments.append(segment)
-            else:
-                # FIXME: doesnt seem to work
-                # we'll also play notes for other segments...
-                note.segments = self.segments_for_landmark(track_note.landmark)
-                segment = note.segments[0]
-
-            if track_note.at:
-                snippet = track_note.at
-            elif track_note.finish_at:
-                snippet = track_note.finish_at
-                finish_at = True
-            else:
-                snippet = "brake_point() or throttle_point()"
-                finish_at = True
-
-            at = self.eval_at(snippet, segment)
-
-            if not at:
-                # FIXME bubble up to UI
-                self.log_error(f"DISCARDING NOTE: note {track_note} has no at - data {note}")
-                continue
-
-            # build the response
-            max_distance_delta = self.max_distance_delta(segment)
-            if finish_at:
-                response = self.build_response(track_note.message, finish_at=at, max_distance_delta=max_distance_delta)
-            else:
-                response = self.build_response(track_note.message, at=at, max_distance_delta=max_distance_delta)
-            note.response = response
-            note.at = response.at
-
-            distance = response.at
-            if distance not in self.distance_notes:
-                self.distance_notes[distance] = []
-            self.distance_notes[distance].append(note)
-
-            for segment in note.segments:
-                if segment not in self.segment_notes:
-                    self.segment_notes[segment] = []
-                self.segment_notes[segment].append(note)
-
-    def init_recon_notes(self, segment=None):
-        if segment is None:
-            self.recon_segment_notes = {}
-            self.recon_distance_notes = {}
-
-        recon_notes = []
-        for segment_key, notes in self.segment_notes.items():
-            if segment and segment_key != segment:
-                continue
-            self.recon_segment_notes[segment_key] = notes.copy()
-            recon_notes.extend(notes)
-
-        for note in recon_notes:
-            distance = note.at
-            if distance not in self.recon_distance_notes:
-                self.recon_distance_notes[distance] = []
-            self.recon_distance_notes[distance].append(note)
-
-    def get_recon_note(self, distance, blast=1):
-        for blast_distance in range(distance - blast, distance + blast + 1):
-            blast_distance = self.distance_add(blast_distance, 0)
-
-            note = self.get_recon_note_at(blast_distance)
-            if note:
-                return note
-
-    def get_recon_note_at(self, distance):
-        notes = self.recon_distance_notes.get(distance, [])
-        note = None
-        if notes:
-            note = notes.pop(0)
-            # self.log_error(f"LEN seg: {len(note.segments)}")
-            for segment in note.segments:
-                # self.log_error(f"LEN: {len(self.recon_segment_notes[segment])}")
-                if note in self.recon_segment_notes[segment]:
-                    self.recon_segment_notes[segment].remove(note)
-                if len(self.recon_segment_notes[segment]) == 0:
-                    self.log_debug(f"reset recon notes for segment {segment}")
-                    self.init_recon_notes(segment=segment)
-        return note
+    def init_recon_notes(self, distance):
+        distance = self.distance_add(distance, self.RECON_ADD_DISTANCE)
+        for turn in self.turns.values():
+            turn.reset(distance)
 
     def tick(self):
         if not self.track_guide:
@@ -223,6 +177,7 @@ class TrackGuideApplication(Application):
         if self.changed_coaching_style():
             if self.is_recon_laps():
                 self.send_response("You are driving recon laps")
+                self.init_recon_notes(self.distance)
             else:
                 self.send_response("You are driving race pace")
 
@@ -232,25 +187,23 @@ class TrackGuideApplication(Application):
             self.respond_pace()
 
     def is_recon_laps(self):
-        return self.recon_laps
+        return self.mode == self.MODE_RECON
 
     def changed_coaching_style(self):
+        previous_mode = self.mode
         if self.is_recon_laps() and self.avg_speed_pct > 0.9:
-            self.recon_laps = False
-            return True
+            self.mode = self.MODE_PACE
         elif not self.is_recon_laps() and self.avg_speed_pct < 0.9:
-            self.recon_laps = True
-            return True
-        return False
+            self.mode = self.MODE_RECON
+        return self.mode != previous_mode
 
     def respond_pace(self):
         pass
 
     def respond_recon(self):
-        add_distance = 100
-        distance = self.distance_add(self.distance, add_distance)
-        if self.message_playing_at(distance):
-            return
+        distance = self.distance_add(self.distance, self.RECON_ADD_DISTANCE)
+        # if self.message_playing_at(distance):
+        #     return
 
         for turn in self.turns.values():
             if turn.is_in_turn(distance):
@@ -259,30 +212,126 @@ class TrackGuideApplication(Application):
                     self.send_response(response)
                     break
 
-    def respond_recon_old(self):
-        # FIXME: seconds_lookahead should be constant, but depend on recon or pace
-        seconds_lookahead = 3  # 10 is the timeout for a queued message
-        add_distance = int(self.telemetry.get("SpeedMs", 50) * seconds_lookahead)
-        distance = self.distance_add(self.distance, add_distance)
-        # self.log_debug(f"respond_recon: {self.distance} -> {distance} (+{add_distance})")
-        if self.message_playing_at(distance):
-            return
-        # a dynamic lookahead means, we could miss some messages
-        # therefore we have a blast radius at self.get_recon_note
-        note = self.get_recon_note(distance)
-        if note:
-            if note.response.message.startswith("Exit wide to the grass - be careful of"):
-                pass
-            self.send_response(note.response)
-
-            # if -100 < (response.at - self.distance) < 0:
-            #     self.log_error(f"response {response} cant be played")
-
     def on_reset_to_pits(self, distance: int, telemetry: dict, now: datetime.datetime):
         self.send_response("You reset to pits - I reset messages")
+        if self.is_recon_laps():
+            self.init_recon_notes(distance)
 
     def on_new_lap(self, distance: int, telemetry: dict, now: datetime.datetime):
         pass
 
     def on_crash(self, distance: int, telemetry: dict, now: datetime.datetime):
         self.send_response("You crashed - drive a bit slower")
+
+    # def init_notes(self):
+    #     self.distance_notes = {}
+    #     self.segment_notes = {}
+    #     # store notes by segment / landmarks
+    #     for track_note in self.track_guide.notes.all():
+    #         note = Note(track_note)
+    #         finish_at = False
+
+    #         if track_note.segment:
+    #             segment = self.get_segment(turn=track_note.segment)
+    #             note.segments.append(segment)
+    #         else:
+    #             # FIXME: doesnt seem to work
+    #             # we'll also play notes for other segments...
+    #             note.segments = self.segments_for_landmark(track_note.landmark)
+    #             segment = note.segments[0]
+
+    #         if track_note.at:
+    #             snippet = track_note.at
+    #         elif track_note.finish_at:
+    #             snippet = track_note.finish_at
+    #             finish_at = True
+    #         else:
+    #             snippet = "brake_point() or throttle_point()"
+    #             finish_at = True
+
+    #         at = self.eval_at(snippet, segment)
+
+    #         if not at:
+    #             # FIXME bubble up to UI
+    #             self.log_error(f"DISCARDING NOTE: note {track_note} has no at - data {note}")
+    #             continue
+
+    #         # build the response
+    #         max_distance_delta = self.max_distance_delta(segment)
+    #         if finish_at:
+    #             response = self.build_response(track_note.message,
+    # finish_at=at, max_distance_delta=max_distance_delta)
+    #         else:
+    #             response = self.build_response(track_note.message, at=at, max_distance_delta=max_distance_delta)
+    #         note.response = response
+    #         note.at = response.at
+
+    #         distance = response.at
+    #         if distance not in self.distance_notes:
+    #             self.distance_notes[distance] = []
+    #         self.distance_notes[distance].append(note)
+
+    #         for segment in note.segments:
+    #             if segment not in self.segment_notes:
+    #                 self.segment_notes[segment] = []
+    #             self.segment_notes[segment].append(note)
+
+    # def init_recon_notes(self, segment=None):
+    #     if segment is None:
+    #         self.recon_segment_notes = {}
+    #         self.recon_distance_notes = {}
+
+    #     recon_notes = []
+    #     for segment_key, notes in self.segment_notes.items():
+    #         if segment and segment_key != segment:
+    #             continue
+    #         self.recon_segment_notes[segment_key] = notes.copy()
+    #         recon_notes.extend(notes)
+
+    #     for note in recon_notes:
+    #         distance = note.at
+    #         if distance not in self.recon_distance_notes:
+    #             self.recon_distance_notes[distance] = []
+    #         self.recon_distance_notes[distance].append(note)
+
+    # def get_recon_note(self, distance, blast=1):
+    #     for blast_distance in range(distance - blast, distance + blast + 1):
+    #         blast_distance = self.distance_add(blast_distance, 0)
+
+    #         note = self.get_recon_note_at(blast_distance)
+    #         if note:
+    #             return note
+
+    # def get_recon_note_at(self, distance):
+    #     notes = self.recon_distance_notes.get(distance, [])
+    #     note = None
+    #     if notes:
+    #         note = notes.pop(0)
+    #         # self.log_error(f"LEN seg: {len(note.segments)}")
+    #         for segment in note.segments:
+    #             # self.log_error(f"LEN: {len(self.recon_segment_notes[segment])}")
+    #             if note in self.recon_segment_notes[segment]:
+    #                 self.recon_segment_notes[segment].remove(note)
+    #             if len(self.recon_segment_notes[segment]) == 0:
+    #                 self.log_debug(f"reset recon notes for segment {segment}")
+    #                 self.init_recon_notes(segment=segment)
+    #     return note
+
+    # def respond_recon_old(self):
+    #     # FIXME: seconds_lookahead should be constant, but depend on recon or pace
+    #     seconds_lookahead = 3  # 10 is the timeout for a queued message
+    #     add_distance = int(self.telemetry.get("SpeedMs", 50) * seconds_lookahead)
+    #     distance = self.distance_add(self.distance, add_distance)
+    #     # self.log_debug(f"respond_recon: {self.distance} -> {distance} (+{add_distance})")
+    #     if self.message_playing_at(distance):
+    #         return
+    #     # a dynamic lookahead means, we could miss some messages
+    #     # therefore we have a blast radius at self.get_recon_note
+    #     note = self.get_recon_note(distance)
+    #     if note:
+    #         if note.response.message.startswith("Exit wide to the grass - be careful of"):
+    #             pass
+    #         self.send_response(note.response)
+
+    #         # if -100 < (response.at - self.distance) < 0:
+    #         #     self.log_error(f"response {response} cant be played")
