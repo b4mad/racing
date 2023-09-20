5211883319c669bf9bd29bcd5afe9e8c5b6a0c69
Christoph Görn <goern@redhat.com>
2023-09-20 16:27:17 +0200

    test(influxdb2): changed default protocol to http, update tests (#470)
    * test(influxdb2): changed default protocol to http, update tests
    
    Signed-off-by: Christoph Görn <goern@b4mad.net>
    
    * test(pytest): just run the unittest when running pytest
    
    Signed-off-by: Christoph Görn <goern@b4mad.net>
    
    * test(python): use the same container image with tekton, as we do with the Containerfile
    
    Signed-off-by: Christoph Görn <goern@b4mad.net>
    
    ---------
    
    Signed-off-by: Christoph Görn <goern@b4mad.net>

--- components/paddock/telemetry/tests/test_influxdb2_config.py
+++ components/paddock/telemetry/tests/test_influxdb2_config.py
@@ -8,12 +8,13 @@ from telemetry.utils import get_influxdb2_config
 
 @pytest.mark.unittest
 class TestInfluxDB2Config:
-    def test_getinfluxdb2_config(self):
+    def test_missing_token(self):
         with pytest.raises(RuntimeEnvironmentConfigurationIncompleteError) as excinfo:
             (org, token, url) = get_influxdb2_config()
             assert org == "b4mad"
         assert str(excinfo.value) == "missing environment variable: B4MAD_RACING_INFLUX_TOKEN"
 
+    def test_missing_host(self):
         with pytest.raises(RuntimeEnvironmentConfigurationIncompleteError) as excinfo:
             os.environ["B4MAD_RACING_INFLUX_TOKEN"] = "test"
 
@@ -24,6 +25,7 @@ class TestInfluxDB2Config:
 
         assert str(excinfo.value) == "missing environment variable: INFLUXDB2_SERVICE_HOST"
 
+    def test_all_good(self):
         os.environ["B4MAD_RACING_INFLUX_ORG"] = "test"
         os.environ["B4MAD_RACING_INFLUX_TOKEN"] = "test"
         os.environ["INFLUXDB2_SERVICE_HOST"] = "test"
@@ -32,4 +34,4 @@ class TestInfluxDB2Config:
 
         assert org == "test"
         assert token == "test"
-        assert url == "https://test:8086"
+        assert url == "http://test:8086/"
