0975acb62b9bbee697f670739779dc5a60d59a1a
Christoph Görn <goern@redhat.com>
2023-09-22 08:53:19 +0200

    read version from pyproject toml (#473)
    * ➗ refactor(paddock): refined the resources of the deployment
    
    Signed-off-by: Christoph Görn <goern@b4mad.net>
    
    * 🧼 correction(commitizen): remove commitizen from the dev environment
    
    Signed-off-by: Christoph Görn <goern@b4mad.net>
    
    * 🎉 feat: paddock is reading its version string from pyproject.toml now
    
    Signed-off-by: Christoph Görn <goern@b4mad.net>
    
    ---------
    
    Signed-off-by: Christoph Görn <goern@b4mad.net>

--- components/paddock/paddock/__init__.py
+++ components/paddock/paddock/__init__.py
@@ -1,3 +1,15 @@
+import toml
+
 from .celery import app as celery_app
+
+
+def get_version_from_pyproject():
+    with open("pyproject.toml", "r") as file:
+        pyproject = toml.load(file)
+        return pyproject["project"]["version"]
+
+
+__version__ = get_version_from_pyproject()
+
 
 __all__ = ("celery_app",)
