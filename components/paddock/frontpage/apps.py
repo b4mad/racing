e3dc7ee2c59ff38f148e96b2622c367c944f91fb
Marcel Hild <hild@b4mad.net>
2023-12-15 23:44:20 +0100

    copy from codeberg

---
+++ components/paddock/frontpage/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class FrontpageConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "frontpage"
---
+++ components/paddock/frontpage/views.py
@@ -0,0 +1,12 @@
+from b4mad_racing_telemetry.models import Session
+from django.views.generic.base import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = "site/home.html"
+
+    # add the 15 most current sessions to the context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["sessions"] = Session.objects.order_by("-modified")[:15]
+        return context
