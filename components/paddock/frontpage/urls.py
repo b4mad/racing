647819f47b214ac8976b1af2162608b473a92b12
Christoph Görn <goern@b4mad.net>
2024-01-11 08:21:45 +0100

    feat: introduce new help pages
    Signed-off-by: Christoph Görn <goern@b4mad.net>

--- components/paddock/frontpage/urls.py
+++ components/paddock/frontpage/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+from frontpage.views import AboutPageView, HelpInstallPageView, HelpPageView, HomePageView
+
+urlpatterns = [
+    path("", HomePageView.as_view(), name="home"),
+    path("about/", AboutPageView.as_view(), name="about"),
+    path("help/", HelpPageView.as_view(), name="help"),
+    path("help/install", HelpInstallPageView.as_view(), name="help-install"),
+]
